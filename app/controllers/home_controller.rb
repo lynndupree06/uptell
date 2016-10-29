472332d53cf4f492caab8fa58c49faa41f4689c4
Jessica B. Kazembe <jbrazelt@thoughtworks.com>
2016-10-29 12:40:31 -0500

    Added help and educators pages to app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def educators
+  end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+  get 'about' => 'home#about'
+  get 'help' => 'home#help'
+  get 'educators' => 'home#educators'
 end
